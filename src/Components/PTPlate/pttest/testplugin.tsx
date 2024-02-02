f6343fffb487521297606b4424a62762bde31103
Pawel Wujczyk <pwujczyk@hotmail.com>
2024-02-02 21:02:35 +0100

    test plugin

--- src/Components/PTPlate/pttest/testplugin.tsx
+++ src/Components/PTPlate/pttest/testplugin.tsx
@@ -1,13 +1,21 @@
 import { createPluginFactory,getPluginType,ELEMENT_DEFAULT,getPluginOptions } from "@udecode/plate";
 
 
-export const KEY_ALIGN = 'align';
+export const KEY_ALIGN = 'XXXX';
  
 export const createAlignPlugin = createPluginFactory({
   key: KEY_ALIGN,
- 
+  handlers: {
+    onChange: (editor) => (value) => {
+      console.info(editor, value);
+    },
+    onKeyDown: (editor) => (event) => {
+      console.info(`You pressed ${event.key}`);
+    },
+  },
   // Note that we're using `then` to access to the editor
   then: (editor) => ({
+    renderAboveEditable:({children})=>(<div><span>{children}</span><span>ddd</span></div>),
     inject: {
       props: {
         nodeKey: KEY_ALIGN,
